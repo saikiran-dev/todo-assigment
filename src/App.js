a4b354386f88a07c0de4f59dd4251f36dac78be1
Aayushi <aayushi@ormae.com>
2021-02-17 16:04:46 +0530

    Added toggle button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import { Table, Switch } from "antd";
-
+import styles from "./App.css";
 const App = () => {
   const [tableDataSource, setTableDataSource] = useState([]);
-  const [taskCompleted, setTaskCompleted] = useState();
-  console.log(tableDataSource);
+  const [taskCompleted, setTaskCompleted] = useState([]);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos?_start=0&_limit=10")
@@ -18,33 +17,30 @@ const App = () => {
       dataIndex: "completed",
       key: "completed",
       render: (text, record, index) => {
-        return <Switch onChange={toggleTask} />;
+        return (
+          <Switch
+            disabled={taskCompleted.includes(index)}
+            onChange={(e) => toggleTask(e, text, record, index)}
+          />
+        );
       },
     },
     {
       title: "Title",
       dataIndex: "title",
       key: "title",
+      render: (text, record, index) => {
+        return taskCompleted.includes(index) ? (
+          <del>{text}</del>
+        ) : (
+          <span>{text}</span>
+        );
+      },
     },
   ];
 
-  const dataSource = [
-    {
-      key: "1",
-      name: "Mike",
-      age: 32,
-      address: "10 Downing Street",
-    },
-    {
-      key: "2",
-      name: "John",
-      age: 42,
-      address: "10 Downing Street",
-    },
-  ];
-
-  const toggleTask = (text, record, index) => {
-    console.log(text, record, index);
+  const toggleTask = (e, text, record, index) => {
+    setTaskCompleted(taskCompleted.concat(index));
   };
   return (
     <Table
